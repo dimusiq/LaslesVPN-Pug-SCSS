75d882e49e487ac7c5616695d600129ca1e8cd2a
dimusiq <dumojke@gmail.com>
2022-11-01 01:02:41 +0500

    banner btn fix

--- gulpfile.js
+++ gulpfile.js
@@ -15,25 +15,31 @@ gulp-concat
 */
 
 global.$ = {
-    gulp: require('gulp'),
-    gp: require('gulp-load-plugins')(),
-    bs: require('browser-sync').create(),
-    sass: require('gulp-sass')(require('sass')),
+	gulp: require('gulp'),
+	gp: require('gulp-load-plugins')(),
+	bs: require('browser-sync').create(),
+	sass: require('gulp-sass')(require('sass')),
 
-    path: {
-        tasks: require('./gulp/config/tasks.js')
-    }
+	path: {
+		tasks: require('./gulp/config/tasks.js'),
+	},
 };
 
 $.path.tasks.forEach(function (taskPath) {
-    require(taskPath)();
+	require(taskPath)();
 });
 
-$.gulp.task('default',$.gulp.series(
-    $.gulp.parallel('pug','sass','scripts:lib','scripts','img','fonts'),
-    $.gulp.parallel('watch','serve')
-));
+$.gulp.task(
+	'default',
+	$.gulp.series(
+		$.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts', 'img', 'fonts'),
+		$.gulp.parallel('watch', 'serve')
+	)
+);
 
-$.gulp.task('build',$.gulp.series(
-    $.gulp.parallel('pug','sass','scripts:lib','scripts','img','fonts')
-));
+$.gulp.task(
+	'build',
+	$.gulp.series(
+		$.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts', 'img', 'fonts')
+	)
+);
