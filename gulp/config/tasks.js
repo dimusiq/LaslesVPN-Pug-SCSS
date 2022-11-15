7c39b0fe74134b0b54761b2c450d098a0a7c8af2
dimusiq <dumojke@gmail.com>
2022-11-15 20:46:59 +0500

    updated build

--- gulp/config/tasks.js
+++ gulp/config/tasks.js
@@ -1,9 +1,9 @@
 module.exports = [
-    './gulp/tasks/pug',
-    './gulp/tasks/serve',
-    './gulp/tasks/sass',
-    './gulp/tasks/watch',
-    './gulp/tasks/script',
-    './gulp/tasks/img',
-    './gulp/tasks/fonts'
+	'./gulp/tasks/pug',
+	'./gulp/tasks/serve',
+	'./gulp/tasks/sass',
+	'./gulp/tasks/watch',
+	'./gulp/tasks/script',
+	'./gulp/tasks/img',
+	'./gulp/tasks/fonts',
 ];
